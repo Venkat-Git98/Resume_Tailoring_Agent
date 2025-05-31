6129c92bd7fe09fd4af2e68416db814f40c7349c
Venkat <svenkatesh.js@gmail.com>
2025-05-30 20:11:43 -0400

    linkedln data scientist link added

--- scrape.py
+++ scrape.py
@@ -143,6 +143,11 @@ SCRAPER_CONFIGS = getattr(app_config, "SCRAPER_CONFIGS", [
         "search_name": "Machine Learning Engineer (LinkedIn - USA, Last 24h)"
     },
     {
+        "platform": "linkedin",
+        "url": "https://www.linkedin.com/jobs/search/?keywords=Data%20Scientist&location=United%20States&geoId=103644278&f_TPR=r3600&f_E=1%2C2%2C3&position=1&pageNum=0",
+        "search_name": "Data Scientist (LinkedIn - USA, Last 24h)"
+    },
+    {
         "platform": "jobright",
         "search_name": "Jobright Recommended Jobs",
         "target_job_count": 10,
